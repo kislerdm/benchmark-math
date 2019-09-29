b72589f9a4d4baace31b28b4f3782ada0990d85f
Dmitry Kisler <admin@dkisler.de>
2019-09-29 17:44:42 +0200

    adjust elapsed time count

--- code/r/bench.R
+++ code/r/bench.R
@@ -18,24 +18,29 @@ input <- sample(0:random_max, length, replace = TRUE)
 # multiplication
 t0 <- Sys.time()
 output <- input * 2.
-cat(paste0("Multiplication by 2: ", (Sys.time() - t0)*1e3, "ms\n"))
+t <- (Sys.time() - t0) * 1e3
+cat(paste0("Multiplication by 2: ", t, "ms\n"))
 
 # sum
 t0 <- Sys.time()
 output <- input + input[c(2:length, 1)]
-cat(paste0("Sum: ", (Sys.time() - t0)*1e3, "ms\n"))
+t <- (Sys.time() - t0) * 1e3
+cat(paste0("Sum: ", t, "ms\n"))
 
 # power
 t0 <- Sys.time()
 output <- input^2
-cat(paste0("Power of 2: ", (Sys.time() - t0)*1e3, "ms\n"))
+t <- (Sys.time() - t0) * 1e3
+cat(paste0("Power of 2: ", t, "ms\n"))
 
 # sigmoid
 t0 <- Sys.time()
 output <- sigmoid(input)
-cat(paste0("Sigmoid: ", (Sys.time() - t0)*1e3, "ms\n"))
+t <- (Sys.time() - t0) * 1e3
+cat(paste0("Sigmoid: ", t, "ms\n"))
 
 # tanh
 t0 <- Sys.time()
 output <- tanh(input)
-cat(paste0("Tanh: ", (Sys.time() - t0)*1e3, "ms\n"))
+t <- (Sys.time() - t0) * 1e3
+cat(paste0("Tanh: ", t, "ms\n"))
